b6d8710b21221646016c45efae569ddbafa4581f
Emal Nawabi <emalnawabi2020@gmail.com>
2023-08-14 01:42:20 +0430

    css coding manipulated

--- components/header-section.js
+++ components/header-section.js
@@ -16,12 +16,30 @@ export default function HeaderSection({ activePage }) {
             <nav className='flex justify-end'>
               <ul className='flex items-center'>
                 <li className='pr-6 last:pr-0 py-10'>
-                  <Link href='/'>Home</Link>
+                  <Link
+                    href='/'
+                    className={activePage === 'home' ? 'text-primary' : 'hover:text-primary'}
+                  >
+                    Home
+                  </Link>
                 </li>
                 <li className='pr-6 last:pr-0 py-10 group'>
                   <Link href='#' className='relative'>
                     <div className='flex items-center justify-center'>
-                      <span className='mr-1 hover:text-primary'>Our Work</span>
+                      <span
+                        className={`mr-1 ${
+                          activePage === 'education' ||
+                          activePage === 'emergency-response' ||
+                          activePage === 'health' ||
+                          activePage === 'water' ||
+                          activePage === 'sport' ||
+                          activePage === 'environment'
+                            ? 'text-primary'
+                            : 'hover:text-primary'
+                        }`}
+                      >
+                        Our Work
+                      </span>
                       <i className='fas fa-angle-down'></i>
                     </div>
                     <div className='hidden absolute -left-[204px] top-0 z-10 p-8 mt-16 w-[25rem] leading-relaxed text-accents_0 shadow-2xl bg-accents_9 group-hover:block group-hover:opacity-100'>
@@ -33,22 +51,70 @@ export default function HeaderSection({ activePage }) {
                           <div className=''>
                             <ul className='text-medium normal-case'>
                               <li className='border-b border-accents_7 last:border-b-0 py-4'>
-                                <Link href='/education'>Education</Link>
+                                <Link
+                                  href='/education'
+                                  className={
+                                    activePage === 'education'
+                                      ? 'text-primary'
+                                      : 'hover:text-primary'
+                                  }
+                                >
+                                  Education
+                                </Link>
                               </li>
                               <li className='border-b border-accents_7 last:border-b-0 py-4'>
-                                <Link href='/emergency-response'>Emergency Response</Link>
+                                <Link
+                                  href='/emergency-response'
+                                  className={
+                                    activePage === 'emergency-reponse'
+                                      ? 'text-primary'
+                                      : 'hover:text-primary'
+                                  }
+                                >
+                                  Emergency Response
+                                </Link>
                               </li>
                               <li className='border-b border-accents_7 last:border-b-0 py-4'>
-                                <Link href='/health'>Health</Link>
+                                <Link
+                                  href='/health'
+                                  className={
+                                    activePage === 'health' ? 'text-primary' : 'hover:text-primary'
+                                  }
+                                >
+                                  Health
+                                </Link>
                               </li>
                               <li className='border-b border-accents_7 last:border-b-0 py-4'>
-                                <Link href='/water'>Water</Link>
+                                <Link
+                                  href='/water'
+                                  className={
+                                    activePage === 'water' ? 'text-primary' : 'hover:text-primary'
+                                  }
+                                >
+                                  Water
+                                </Link>
                               </li>
                               <li className='border-b border-accents_7 last:border-b-0 py-4'>
-                                <Link href='/sport'>Sport</Link>
+                                <Link
+                                  href='/sport'
+                                  className={
+                                    activePage === 'sport' ? 'text-primary' : 'hover:text-primary'
+                                  }
+                                >
+                                  Sport
+                                </Link>
                               </li>
                               <li className='border-b border-accents_7 last:border-b-0 py-4'>
-                                <Link href='/environment'>Environment</Link>
+                                <Link
+                                  href='/environment'
+                                  className={
+                                    activePage === 'environment'
+                                      ? 'text-primary'
+                                      : 'hover:text-primary'
+                                  }
+                                >
+                                  Environment
+                                </Link>
                               </li>
                             </ul>
                           </div>
@@ -58,12 +124,25 @@ export default function HeaderSection({ activePage }) {
                   </Link>
                 </li>
                 <li className='pr-6 last:pr-0 py-10'>
-                  <Link href='/gallery'>Gallery</Link>
+                  <Link
+                    href='/gallery'
+                    className={activePage === 'galley' ? 'text-primary' : 'hover:text-primary'}
+                  >
+                    Gallery
+                  </Link>
                 </li>
                 <li className='pr-6 last:pr-0 py-10 group'>
                   <Link href='#' className='relative'>
                     <div className='flex items-center justify-center'>
-                      <span className='mr-1 hover:text-primary'>News & Stories</span>
+                      <span
+                        className={`mr-1 ${
+                          activePage === 'latest-news' || activePage === 'video-library'
+                            ? 'text-primary'
+                            : 'hover:text-primary'
+                        }`}
+                      >
+                        News & Stories
+                      </span>
                       <i className='fas fa-angle-down'></i>
                     </div>
                     <div className='hidden absolute -left-[204px] top-0 z-10 p-8 mt-16 w-[25rem] leading-relaxed text-accents_0 shadow-2xl bg-accents_9 group-hover:block group-hover:opacity-100'>
@@ -75,10 +154,28 @@ export default function HeaderSection({ activePage }) {
                           <div className=''>
                             <ul className='text-medium normal-case'>
                               <li className='border-b border-accents_7 last:border-b-0 py-4'>
-                                <Link href='/news/category/latest-news'>Latest News</Link>
+                                <Link
+                                  href='/news/category/latest-news'
+                                  className={
+                                    activePage === 'latest-news'
+                                      ? 'text-primary'
+                                      : 'hover:text-primary'
+                                  }
+                                >
+                                  Latest News
+                                </Link>
                               </li>
                               <li className='border-b border-accents_7 last:border-b-0 py-4'>
-                                <Link href='/faqs/category/video-library'>Video Library</Link>
+                                <Link
+                                  href='/faqs/category/video-library'
+                                  className={
+                                    activePage === 'video-library'
+                                      ? 'text-primary'
+                                      : 'hover:text-primary'
+                                  }
+                                >
+                                  Video Library
+                                </Link>
                               </li>
                             </ul>
                           </div>
@@ -88,10 +185,20 @@ export default function HeaderSection({ activePage }) {
                   </Link>
                 </li>
                 <li className='pr-6 last:pr-0 py-10'>
-                  <Link href='/about'>About Us</Link>
+                  <Link
+                    href='/about'
+                    className={activePage === 'about' ? 'text-primary' : 'hover:text-primary'}
+                  >
+                    About Us
+                  </Link>
                 </li>
                 <li className='pr-6 last:pr-0 py-10'>
-                  <Link href='/contact'>Contact</Link>
+                  <Link
+                    href='/contact'
+                    className={activePage === 'contact' ? 'text-primary' : 'hover:text-primary'}
+                  >
+                    Contact
+                  </Link>
                 </li>
               </ul>
               <div className='ml-7 mb-1'>
